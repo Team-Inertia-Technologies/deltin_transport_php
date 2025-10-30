3c8a5a04d97e20ea4581c24b8299b82db803f380
Myron <myron@teaminertia.com>
2025-10-30 10:41:25 +0530

    logout
    Signed-off-by: Myron <myron@teaminertia.com>

--- public/api/config/logout.php
+++ public/api/config/logout.php
@@ -1,10 +1,11 @@
 <?php
-// error_reporting(E_ALL);
-// ini_set('display_errors', '1');
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 $NO_REDIRECT = $NO_PRELOAD = 1;
 include "../../includes/common_api.php";
-
-UpdateField('users', 'cActive', 'N', " iUserID=$sess_user_id");
+$token = isset($_POST['token']) ? db_input($_POST['token']) : '';
+$user_id = DecodeParam($token);
+sql_query("UPDATE users SET cActive='N' WHERE iUserID=$user_id");
 
 session_destroy();
 $response = array('statusCode' => 200, 'message' => "Logged out successfully");
