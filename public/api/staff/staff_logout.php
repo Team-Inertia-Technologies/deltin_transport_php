297a9259a0bcefd227388536e0c68cc16f6f737d
Shivani <shivani@teaminertia.com>
2025-11-10 12:08:15 +0530

    sfaff logout

---
+++ public/api/staff/staff_logout.php
@@ -0,0 +1,15 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+$NO_REDIRECT = $NO_PRELOAD = 1;
+include "../../includes/common_api.php";
+$token = isset($_POST['token']) ? db_input($_POST['token']) : '';
+$user_id = DecodeParam($token);
+sql_query("UPDATE users SET cActive='N' WHERE iUserID=$user_id");
+
+$response = array('statusCode' => 200, 'message' => "Logged out successfully");
+http_response_code(200);
+header('Content-Type: application/json');
+echo json_encode($response);
+exit;
+?>
