7228a3ac2a1df05c0e0660defb91ac1b1d2cddec
Shivani <shivani@teaminertia.com>
2025-11-10 16:26:07 +0530

    logout

--- public/api/staff/staff_logout.php
+++ public/api/staff/staff_logout.php
@@ -3,13 +3,22 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 $NO_REDIRECT = $NO_PRELOAD = 1;
 include "../../includes/common_api.php";
+
 $token = isset($_POST['token']) ? db_input($_POST['token']) : '';
 $user_id = DecodeParam($token);
-sql_query("UPDATE staff SET cActive='N' WHERE iStaffID=$user_id");
+
+// Validate user_id before using in SQL query
+if (empty($user_id) || !is_numeric($user_id)) {
+    $response = ['statusCode' => 400, 'message' => "Invalid token"];
+    http_response_code(400);
+    header('Content-Type: application/json');
+    echo json_encode($response);
+    exit;
+}
+
+sql_query("UPDATE staff SET cActive='N' WHERE iStaffID=" . intval($user_id));
 
 $response = ['statusCode' => 200, 'message' => "Logged out successfully"];
 http_response_code(200);
 header('Content-Type: application/json');
 echo json_encode($response);
-exit;
-?>
