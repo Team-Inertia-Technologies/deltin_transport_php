eb050d781d6f3145f3ceec0b25f5a82ee19a4733
Myron <myron@teaminertia.com>
2025-12-15 10:48:08 +0530

    modified
    Signed-off-by: Myron <myron@teaminertia.com>

--- public/api/driver/driver_note.php
+++ public/api/driver/driver_note.php
@@ -22,6 +22,7 @@ $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 
 $token      = trim($request->token);
+$iVehicleID = intval($request->VehicleID);
 $reviewType = trim($request->reviewType);
 $reviewText = trim($request->reviewText);
 
@@ -61,7 +62,7 @@ $driverID = intval($userid);
 // -------------------- INSERT DRIVER NOTE --------------------
 $id = NextID('iNoteID', 'driver_notes');
 $NOW = NOW;
-$sql = "INSERT INTO driver_notes (iNoteID, iDriverID, cType, vText, dtCreated) VALUES ($id,'$driverID', '$reviewType', '$reviewText', '$NOW')";
+$sql = "INSERT INTO driver_notes (iNoteID, iDriverID, iVehicleID, cType, vText, dtCreated) VALUES ($id,'$driverID', '$iVehicleID', '$reviewType', '$reviewText', '$NOW')";
 $result = sql_query($sql, 'DRIVER.NOTE.INSERT');
 if (sql_affected_rows() > 0) {
 	http_response_code(200);
