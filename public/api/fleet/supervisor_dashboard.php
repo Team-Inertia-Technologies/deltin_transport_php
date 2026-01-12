f8c69409248e2a74fa8aa1842cc4e8a9640fd594
david_inertia <david@teaminertia.com>
2026-01-12 11:40:05 +0530

    Fixing the output of the vehicle categories

--- public/api/fleet/supervisor_dashboard.php
+++ public/api/fleet/supervisor_dashboard.php
@@ -31,7 +31,7 @@ switch ($mode) {
 	// ===================== CASE: DASHBOARD_COMPONENTS =====================	
     case 'DASHBOARD_COMPONENTS':
 	
-		$VEH_CAT = GetXArrFromYID("SELECT iVCatID, vName from vehicle_category where cStatus='A' AND cType IN ('F') ORDER BY iRank", "3");
+		$VEH_CAT = GetXArrFromYID("SELECT iVCatID, vName from vehicle_category where cStatus='A' AND cType IN ('F','B') ORDER BY iRank", "3");
 		$TODAY = date('Y-m-d');
 		$TOTAL_VEHICLE_COUNT = GetXFromYID("select count(*) from vehicle where cStatus = 'A' and cServiceType IN ('F')");
 		$TOTAL_DRIVER_COUNT = GetXFromYID("select count(*) from driver where cStatus = 'A' and dExpiry > '$TODAY'");
