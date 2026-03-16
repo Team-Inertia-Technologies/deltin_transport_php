4bee4d2ff7290ccd49f6cf638ac0b0a00ba43c42
Shivani <shivani@teaminertia.com>
2026-03-16 16:16:33 +0530

    time offset

--- public/api/staff/request.php
+++ public/api/staff/request.php
@@ -150,6 +150,7 @@ switch ($mode) {
                     "name"      => $routeName,
                     //    "pickUpOpt" => [],        
                     "timeOpt"   => $timeOpt
+            
                 ];
             }
         }
@@ -158,7 +159,8 @@ switch ($mode) {
             "data" => [
                 "routes"       => $routes,
                 "staffRouteID" => $staffRouteID,
-                "staffStopID"  => $staffStopID
+                "staffStopID"  => $staffStopID,
+                "reqOffset"  => $req_offset . " hours"
             ],
             "statusCode" => 200
         ]);
