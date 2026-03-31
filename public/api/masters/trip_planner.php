1b035ffd6a7ec644abc77dd58b94978b02ab331e
Shivani <shivani@teaminertia.com>
2026-03-31 12:24:26 +0530

    order by

--- public/api/masters/trip_planner.php
+++ public/api/masters/trip_planner.php
@@ -618,9 +618,7 @@ if (count($statuses) > 1) {
                        FROM vehicle v
                        LEFT JOIN vehicle_category vc ON v.iCatID = vc.iVCatID AND vc.cStatus = 'A'
                        LEFT JOIN vendor ven ON v.iVendorID = ven.iVendorID AND ven.cStatus = 'A' and ven.cType IN ('B','S') 
-                       WHERE v.cStatus = 'A'
-                       AND v.cServiceType IN ('S','B')
-                       ORDER BY v.vRnum";
+                       WHERE v.cStatus = 'A' AND v.cServiceType IN ('S','B') ORDER BY t.iTripID DESC LIMIT 2";
         $tableArrRes = sql_query($tableArrSql);
 
         $tableArr = [];
